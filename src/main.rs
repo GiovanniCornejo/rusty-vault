f17e69684a8ad91194d7ba29f272d266719d1bd5
Gio <60558512+GiovanniCornejo@users.noreply.github.com>
2024-02-11 01:09:33 -0500

    Added usage display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::result::Result;
+use std::{env, process::ExitCode};
+
+fn usage(program: &str) {
+    eprintln!("Usage: {program} generate [OPTIONS]");
+    eprintln!("\nOptions:");
+    eprintln!("    -h, --help                           show this help message and exit");
+}
+
+fn entry() -> Result<(), ()> {
+    let mut args = env::args();
+    let program = args.next().expect("path to program is provided");
+    let subcommand = args.next().ok_or_else(|| {
+        usage(&program);
+        eprintln!("ERROR: no subcommand is provided");
+    })?;
+
+    Ok(());
+}
+
+fn main() -> ExitCode {
+    match entry() {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(()) => ExitCode::FAILURE,
+    }
 }
