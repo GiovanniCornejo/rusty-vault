46452e66b5f86d6c08abe304eb345335000f4501
Gio <60558512+GiovanniCornejo@users.noreply.github.com>
2024-02-11 03:53:14 -0500

    Added minimum requirements for password generation

--- src/password_generator.rs
+++ src/password_generator.rs
@@ -1,4 +1,4 @@
-use rand::{thread_rng, Rng};
+use rand::{seq::SliceRandom, thread_rng, Rng};
 
 const DEFAULT_LENGTH: std::ops::Range<usize> = 12..17;
 
@@ -9,7 +9,7 @@ const SPECIAL: &str = "!@#$%^&*()";
 
 pub struct PasswordGenerator {
     length: usize,
-    char_set: Vec<char>,
+    char_sets: Vec<Vec<char>>,
     min_uppercase: usize,
     min_lowercase: usize,
     min_digits: usize,
@@ -28,23 +28,23 @@ impl PasswordGenerator {
         min_digits: usize,
         min_special: usize,
     ) -> Self {
-        let mut char_set = String::new();
+        let mut char_sets = Vec::new();
         if uppercase {
-            char_set += UPPERCASE;
+            char_sets.push(UPPERCASE.chars().collect());
         }
         if lowercase {
-            char_set += LOWERCASE;
+            char_sets.push(LOWERCASE.chars().collect());
         }
         if digits {
-            char_set += DIGITS;
+            char_sets.push(DIGITS.chars().collect());
         }
         if special {
-            char_set += SPECIAL;
+            char_sets.push(SPECIAL.chars().collect());
         }
 
         Self {
             length,
-            char_set: char_set.chars().collect(),
+            char_sets,
             min_uppercase,
             min_lowercase,
             min_digits,
@@ -56,11 +56,34 @@ impl PasswordGenerator {
         let mut pw = String::new();
         let mut rng = thread_rng();
 
-        // Generate a random character in the char_set and add to password
-        for _ in 0..self.length {
-            pw.push(self.char_set[rng.gen_range(0..self.char_set.len())]);
+        // Keep track of minimum requirements
+        let mut remaining_length = self.length;
+        let required_counts = [
+            self.min_uppercase,
+            self.min_lowercase,
+            self.min_digits,
+            self.min_special,
+        ];
+
+        // Generate characters for each set until minimum requirements are met
+        for (i, char_set) in self.char_sets.iter().enumerate() {
+            let required_count = required_counts[i].min(remaining_length);
+            for _ in 0..required_count {
+                pw.push(char_set[rng.gen_range(0..char_set.len())]);
+                remaining_length -= 1;
+            }
         }
-        pw
+
+        // Generate remaining characters
+        for _ in 0..remaining_length {
+            let char_set = &self.char_sets[rng.gen_range(0..self.char_sets.len())];
+            pw.push(char_set[rng.gen_range(0..char_set.len())]);
+        }
+
+        // Shuffle the generated password
+        let mut pw: Vec<char> = pw.chars().collect();
+        pw.shuffle(&mut rng);
+        pw.iter().collect()
     }
 }
 
